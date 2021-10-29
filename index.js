0014f2e317a27cd15d5ae5367e53e012f4afaa83
Showkatul islam <showkatul2@gmail.com>
2021-10-29 12:39:57 +0600

    d

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const app=express()
 require('dotenv').config()
-const port=5000
+const port=process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 const uri=`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.xhxaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
